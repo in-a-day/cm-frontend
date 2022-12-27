642ab6a2d26e6c38ac8caab4132f7a4c496d302e
zbs <zhangbshu@gmail.com>
2022-12-27 11:39:51 +0800

    init

---
+++ src/components/Footer/src/Footer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useDesign } from '@/hooks/web/useDesign'
+
+const { getPrefixCls } = useDesign()
+
+const prefixCls = getPrefixCls('footer')
+
+</script>
+
+<template>
+  <div
+    :class="prefixCls"
+    class="text-center text-[var(--el-text-color-placeholder)] bg-[var(--app-content-bg-color)] h-[var(--app-footer-height)] leading-[var(--app-footer-height)] dark:bg-[var(--el-bg-color)]"
+  >
+  </div>
+</template>
